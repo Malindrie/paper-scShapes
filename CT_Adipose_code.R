f857187af20c81a1b4944867c85c8bbdc78e58de
Malindrie Dharmaratne <50472285+Malindrie@users.noreply.github.com>
2022-01-28 15:34:15 +1000

    Update CT_Adipose_code.R

--- CT_Adipose_code.R
+++ CT_Adipose_code.R
@@ -212,177 +212,177 @@ model_Chizinb <- function(x, lib.size, mou.id, cell.id){
 
 
 
-# #########################################################################################################################
-# ####run the script for the three conditions seperately
-# ####load cell-type rds object
-# load("METF/Cell-types/Adipose_celltypes.rds")
-# 
-# #convert to matrix form
-# O_fat_Cells <- subset(Adipose_celltypes, subset = condition == "Old")
-# O_fat_count <- as.matrix(O_fat_Cells@assays$RNA@counts)
-# 
-# ###convert data matrix to sparse matrix
-# O_fat_count <- Matrix(O_fat_count, sparse = TRUE)
-# 
-# #filter genes not expressed in at least 10% of the cells
-# O_fat_filt <- O_fat_count[apply(O_fat_count, 1, function(x){sum(x == 0)} < ncol(O_fat_count)*0.90),]
-# 
-# 
-# ########Old
-# ### calculate library size
-# lib.size <- Library_size(O_fat_filt)
-# 
-# ###calculte mouse ID
-# mou.id <- Mouse_ID(O_fat_filt)
-# 
-# ###calculate cell ID
-# cells <- as.matrix(colnames(O_fat_Cells))
-# cell_types <- as.matrix(O_fat_Cells@meta.data$my_idents)
-# cell.types <- cbind(cells, cell_types)
-# colnames(cell.types) <- c("cells", "cell_type")
-# 
-# cell.id <- cell_type(cell.types)
-# 
-# 
-# plan(multisession, workers = 24)
-# 
-# #perform the KS test
-# O_fat_KS <- future_apply(O_fat_filt, MARGIN = 1L, FUN = KS_ZINB, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# save(O_fat_KS, file = "METF/Cell-types/Adipose/O_fat/O_fat_KS")
-# 
-# #select genes passing the KS test
-# O_fat_KS <- stack(O_fat_KS)
-# O_fat_KS$values <- as.numeric(O_fat_KS$values)
-# O_fat_KS <- O_fat_KS[!is.na(O_fat_KS$values), ]
-# 
-# O_fat_KSig <- O_fat_KS[O_fat_KS$values > 0.05, ]
-# O_fat_genes <- O_fat_KSig$ind
-# 
-# O_fat_data <- O_fat_filt[rownames(O_fat_filt) %in% O_fat_genes,]
-# 
-# 
-# #fit models and save the models
-# O_fat_poi <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_poi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# O_fat_nb <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_nb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# O_fat_zip <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_zip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# O_fat_zinb <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_zinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# 
-# #Calculate BIC
-# O_fat_poi_BIC <- t(as.data.frame(lapply(O_fat_poi, model_BIC)))
-# O_fat_nb_BIC <- t(as.data.frame(lapply(O_fat_nb, model_BIC)))
-# O_fat_zip_BIC <- t(as.data.frame(lapply(O_fat_zip, model_BIC)))
-# O_fat_zinb_BIC <- t(as.data.frame(lapply(O_fat_zinb, model_BIC)))
-# 
-# 
-# O_fat_BIC <- cbind(O_fat_poi_BIC, O_fat_nb_BIC, O_fat_zip_BIC, O_fat_zinb_BIC)
-# colnames(O_fat_BIC) <- c("Poi_BIC", "NB_BIC", "ZIP_BIC", "ZINB_BIC")
-# save(O_fat_BIC, file = "METF/Cell-types/Adipose/O_fat/O_fat_BIC.rds")
-# 
-# 
-# ##convert data matrix to list for selecting the model with the least BIC value
-# O_fat_list <- lapply(as.list(1:dim(O_fat_data)[1]), function(x) O_fat_data[x[1],])
-# names(O_fat_list) <- rownames(O_fat_data)
-# 
-# #Select the model with the least BIC value
-# BM_genes <- Best_model(O_fat_BIC, O_fat_list)
-# 
-# 
-# O_fat_Chipoi_pval <- future_lapply(BM_genes[[1]], model_Chipoi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# O_fat_Chinb_pval <- future_lapply(BM_genes[[2]], model_Chinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# O_fat_Chizip_pval <- future_lapply(BM_genes[[3]], model_Chizip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# O_fat_Chizinb_pval <- future_lapply(BM_genes[[4]], model_Chizinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# 
-# 
-# save(O_fat_Chipoi_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chipoi_pval.rds")
-# save(O_fat_Chinb_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chinb_pval.rds")
-# save(O_fat_Chizip_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chizip_pval.rds")
-# save(O_fat_Chizinb_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chizinb_pval.rds")
-# 
-# 
-# ##################################################################################################
-# ########Young
-# load("METF/Cell-types/Adipose_celltypes.rds")
-# 
-# #convert to matrix form
-# Y_fat_Cells <- subset(Adipose_celltypes, subset = condition == "Young")
-# Y_fat_count <- as.matrix(Y_fat_Cells@assays$RNA@counts)
-# 
-# ###convert data matrix to sparse matrix
-# Y_fat_count <- Matrix(Y_fat_count, sparse = TRUE)
-# 
-# #filter genes not expressed in at least 10% of the cells
-# Y_fat_filt <- Y_fat_count[apply(Y_fat_count, 1, function(x){sum(x == 0)} < ncol(Y_fat_count)*0.90),]
-# 
-# 
-# ########Yld
-# ### calculate library size
-# lib.size <- Library_size(Y_fat_filt)
-# 
-# ###calculte mouse ID
-# mou.id <- Mouse_ID(Y_fat_filt)
-# 
-# ###calculate cell ID
-# cells <- as.matrix(colnames(Y_fat_Cells))
-# cell_types <- as.matrix(Y_fat_Cells@meta.data$my_idents)
-# cell.types <- cbind(cells, cell_types)
-# colnames(cell.types) <- c("cells", "cell_type")
-# 
-# cell.id <- cell_type(cell.types)
-# 
-# 
-# plan(multisession, workers = 24)
-# 
-# #perform the KS test
-# Y_fat_KS <- future_apply(Y_fat_filt, MARGIN = 1L, FUN = KS_ZINB, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# save(Y_fat_KS, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_KS")
-# 
-# #select genes passing the KS test
-# Y_fat_KS <- stack(Y_fat_KS)
-# Y_fat_KS$values <- as.numeric(Y_fat_KS$values)
-# Y_fat_KS <- Y_fat_KS[!is.na(Y_fat_KS$values), ]
-# 
-# Y_fat_KSig <- Y_fat_KS[Y_fat_KS$values > 0.05, ]
-# Y_fat_genes <- Y_fat_KSig$ind
-# 
-# Y_fat_data <- Y_fat_filt[rownames(Y_fat_filt) %in% Y_fat_genes,]
-# 
-# 
-# #fit models and save the models
-# Y_fat_poi <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_poi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# Y_fat_nb <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_nb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# Y_fat_zip <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_zip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# Y_fat_zinb <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_zinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# 
-# #Calculate BIC
-# Y_fat_poi_BIC <- t(as.data.frame(lapply(Y_fat_poi, model_BIC)))
-# Y_fat_nb_BIC <- t(as.data.frame(lapply(Y_fat_nb, model_BIC)))
-# Y_fat_zip_BIC <- t(as.data.frame(lapply(Y_fat_zip, model_BIC)))
-# Y_fat_zinb_BIC <- t(as.data.frame(lapply(Y_fat_zinb, model_BIC)))
-# 
-# 
-# Y_fat_BIC <- cbind(Y_fat_poi_BIC, Y_fat_nb_BIC, Y_fat_zip_BIC, Y_fat_zinb_BIC)
-# colnames(Y_fat_BIC) <- c("Poi_BIC", "NB_BIC", "ZIP_BIC", "ZINB_BIC")
-# save(Y_fat_BIC, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_BIC.rds")
-# 
-# 
-# ##convert data matrix to list for selecting the model with the least BIC value
-# Y_fat_list <- lapply(as.list(1:dim(Y_fat_data)[1]), function(x) Y_fat_data[x[1],])
-# names(Y_fat_list) <- rownames(Y_fat_data)
-# 
-# #Select the model with the least BIC value
-# BM_genes <- Best_model(Y_fat_BIC, Y_fat_list)
-# 
-# 
-# Y_fat_Chipoi_pval <- future_lapply(BM_genes[[1]], model_Chipoi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# Y_fat_Chinb_pval <- future_lapply(BM_genes[[2]], model_Chinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# Y_fat_Chizip_pval <- future_lapply(BM_genes[[3]], model_Chizip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# Y_fat_Chizinb_pval <- future_lapply(BM_genes[[4]], model_Chizinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
-# 
-# 
-# save(Y_fat_Chipoi_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chipoi_pval.rds")
-# save(Y_fat_Chinb_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chinb_pval.rds")
-# save(Y_fat_Chizip_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chizip_pval.rds")
-# save(Y_fat_Chizinb_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chizinb_pval.rds")
+#########################################################################################################################
+####run the script for the three conditions seperately
+####load cell-type rds object
+load("METF/Cell-types/Adipose_celltypes.rds")
+
+#convert to matrix form
+O_fat_Cells <- subset(Adipose_celltypes, subset = condition == "Old")
+O_fat_count <- as.matrix(O_fat_Cells@assays$RNA@counts)
+
+###convert data matrix to sparse matrix
+O_fat_count <- Matrix(O_fat_count, sparse = TRUE)
+
+#filter genes not expressed in at least 10% of the cells
+O_fat_filt <- O_fat_count[apply(O_fat_count, 1, function(x){sum(x == 0)} < ncol(O_fat_count)*0.90),]
+
+
+########Old
+### calculate library size
+lib.size <- Library_size(O_fat_filt)
+
+###calculte mouse ID
+mou.id <- Mouse_ID(O_fat_filt)
+
+###calculate cell ID
+cells <- as.matrix(colnames(O_fat_Cells))
+cell_types <- as.matrix(O_fat_Cells@meta.data$my_idents)
+cell.types <- cbind(cells, cell_types)
+colnames(cell.types) <- c("cells", "cell_type")
+
+cell.id <- cell_type(cell.types)
+
+
+plan(multisession, workers = 24)
+
+#perform the KS test
+O_fat_KS <- future_apply(O_fat_filt, MARGIN = 1L, FUN = KS_ZINB, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+save(O_fat_KS, file = "METF/Cell-types/Adipose/O_fat/O_fat_KS")
+
+#select genes passing the KS test
+O_fat_KS <- stack(O_fat_KS)
+O_fat_KS$values <- as.numeric(O_fat_KS$values)
+O_fat_KS <- O_fat_KS[!is.na(O_fat_KS$values), ]
+
+O_fat_KSig <- O_fat_KS[O_fat_KS$values > 0.05, ]
+O_fat_genes <- O_fat_KSig$ind
+
+O_fat_data <- O_fat_filt[rownames(O_fat_filt) %in% O_fat_genes,]
+ 
+
+#fit models and save the models
+O_fat_poi <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_poi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+O_fat_nb <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_nb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+O_fat_zip <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_zip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+O_fat_zinb <- future_apply(O_fat_data, MARGIN = 1L, FUN = model_zinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+ 
+#Calculate BIC
+O_fat_poi_BIC <- t(as.data.frame(lapply(O_fat_poi, model_BIC)))
+O_fat_nb_BIC <- t(as.data.frame(lapply(O_fat_nb, model_BIC)))
+O_fat_zip_BIC <- t(as.data.frame(lapply(O_fat_zip, model_BIC)))
+O_fat_zinb_BIC <- t(as.data.frame(lapply(O_fat_zinb, model_BIC)))
+ 
+ 
+O_fat_BIC <- cbind(O_fat_poi_BIC, O_fat_nb_BIC, O_fat_zip_BIC, O_fat_zinb_BIC)
+colnames(O_fat_BIC) <- c("Poi_BIC", "NB_BIC", "ZIP_BIC", "ZINB_BIC")
+save(O_fat_BIC, file = "METF/Cell-types/Adipose/O_fat/O_fat_BIC.rds")
+
+
+##convert data matrix to list for selecting the model with the least BIC value
+O_fat_list <- lapply(as.list(1:dim(O_fat_data)[1]), function(x) O_fat_data[x[1],])
+names(O_fat_list) <- rownames(O_fat_data)
+
+#Select the model with the least BIC value
+BM_genes <- Best_model(O_fat_BIC, O_fat_list)
+
+
+O_fat_Chipoi_pval <- future_lapply(BM_genes[[1]], model_Chipoi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+O_fat_Chinb_pval <- future_lapply(BM_genes[[2]], model_Chinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+O_fat_Chizip_pval <- future_lapply(BM_genes[[3]], model_Chizip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+O_fat_Chizinb_pval <- future_lapply(BM_genes[[4]], model_Chizinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+ 
+ 
+save(O_fat_Chipoi_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chipoi_pval.rds")
+save(O_fat_Chinb_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chinb_pval.rds")
+save(O_fat_Chizip_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chizip_pval.rds")
+save(O_fat_Chizinb_pval, file = "METF/Cell-types/Adipose/O_fat/O_fat_Chizinb_pval.rds")
+
+
+##################################################################################################
+########Young
+load("METF/Cell-types/Adipose_celltypes.rds")
+
+#convert to matrix form
+Y_fat_Cells <- subset(Adipose_celltypes, subset = condition == "Young")
+Y_fat_count <- as.matrix(Y_fat_Cells@assays$RNA@counts)
+
+###convert data matrix to sparse matrix
+Y_fat_count <- Matrix(Y_fat_count, sparse = TRUE)
+ 
+#filter genes not expressed in at least 10% of the cells
+Y_fat_filt <- Y_fat_count[apply(Y_fat_count, 1, function(x){sum(x == 0)} < ncol(Y_fat_count)*0.90),]
+ 
+ 
+########Young
+### calculate library size
+lib.size <- Library_size(Y_fat_filt)
+
+###calculte mouse ID
+mou.id <- Mouse_ID(Y_fat_filt)
+ 
+###calculate cell ID
+cells <- as.matrix(colnames(Y_fat_Cells))
+cell_types <- as.matrix(Y_fat_Cells@meta.data$my_idents)
+cell.types <- cbind(cells, cell_types)
+colnames(cell.types) <- c("cells", "cell_type")
+
+cell.id <- cell_type(cell.types)
+
+
+plan(multisession, workers = 24)
+
+#perform the KS test
+Y_fat_KS <- future_apply(Y_fat_filt, MARGIN = 1L, FUN = KS_ZINB, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+save(Y_fat_KS, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_KS")
+
+#select genes passing the KS test
+Y_fat_KS <- stack(Y_fat_KS)
+Y_fat_KS$values <- as.numeric(Y_fat_KS$values)
+Y_fat_KS <- Y_fat_KS[!is.na(Y_fat_KS$values), ]
+
+Y_fat_KSig <- Y_fat_KS[Y_fat_KS$values > 0.05, ]
+Y_fat_genes <- Y_fat_KSig$ind
+
+Y_fat_data <- Y_fat_filt[rownames(Y_fat_filt) %in% Y_fat_genes,]
+
+
+#fit models and save the models
+Y_fat_poi <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_poi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+Y_fat_nb <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_nb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+Y_fat_zip <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_zip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+Y_fat_zinb <- future_apply(Y_fat_data, MARGIN = 1L, FUN = model_zinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+ 
+#Calculate BIC
+Y_fat_poi_BIC <- t(as.data.frame(lapply(Y_fat_poi, model_BIC)))
+Y_fat_nb_BIC <- t(as.data.frame(lapply(Y_fat_nb, model_BIC)))
+Y_fat_zip_BIC <- t(as.data.frame(lapply(Y_fat_zip, model_BIC)))
+Y_fat_zinb_BIC <- t(as.data.frame(lapply(Y_fat_zinb, model_BIC)))
+
+ 
+Y_fat_BIC <- cbind(Y_fat_poi_BIC, Y_fat_nb_BIC, Y_fat_zip_BIC, Y_fat_zinb_BIC)
+colnames(Y_fat_BIC) <- c("Poi_BIC", "NB_BIC", "ZIP_BIC", "ZINB_BIC")
+save(Y_fat_BIC, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_BIC.rds")
+
+
+##convert data matrix to list for selecting the model with the least BIC value
+Y_fat_list <- lapply(as.list(1:dim(Y_fat_data)[1]), function(x) Y_fat_data[x[1],])
+names(Y_fat_list) <- rownames(Y_fat_data)
+
+#Select the model with the least BIC value
+BM_genes <- Best_model(Y_fat_BIC, Y_fat_list)
+ 
+ 
+Y_fat_Chipoi_pval <- future_lapply(BM_genes[[1]], model_Chipoi, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+Y_fat_Chinb_pval <- future_lapply(BM_genes[[2]], model_Chinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+Y_fat_Chizip_pval <- future_lapply(BM_genes[[3]], model_Chizip, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+Y_fat_Chizinb_pval <- future_lapply(BM_genes[[4]], model_Chizinb, lib.size <- lib.size, mou.id <- mou.id, cell.id <- cell.id, future.seed = 0xBEEF)
+ 
+ 
+save(Y_fat_Chipoi_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chipoi_pval.rds")
+save(Y_fat_Chinb_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chinb_pval.rds")
+save(Y_fat_Chizip_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chizip_pval.rds")
+save(Y_fat_Chizinb_pval, file = "METF/Cell-types/Adipose/Y_fat/Y_fat_Chizinb_pval.rds")
 
 
 ##########################################################################################################
